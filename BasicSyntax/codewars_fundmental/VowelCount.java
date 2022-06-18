be6b65c8c81bbe5dd3b21074c041d969f8d3da82
shlee0916 <dkssudxx@naver.com>
2022-06-18 21:09:53 +0900

    FEAT: Update new solution 06/18
    Fundmental solution for Codewars. For practice of Java.

---
+++ BasicSyntax/codewars_fundmental/VowelCount.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+package BasicSyntax.codewars_fundmental;
+
+public class VowelCount {
+    public int getCount(String str) {
+        return str.length() - str.replaceAll("[a|e|i|o|u]", "").length();
+    }
+
+    public static void main(String args[]) {
+        VowelCount vow = new VowelCount();
+
+        System.out.println(vow.getCount("Nope"));
+        System.out.println(vow.getCount("abracadabra"));
+    }
+}
