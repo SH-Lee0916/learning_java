3c58fb2e7e57e8408d7068de8458428dcbb482cb
GitHub-Joayo <dkssudxx@naver.com>
2022-06-22 01:24:16 +0900

    FEAT: Add start day
    Add start day.

--- Calendar/Calendar.java
+++ Calendar/Calendar.java
@@ -1,8 +1,12 @@
 package Calendar;
 
+import java.util.Arrays;
+
 public class Calendar {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
+    private static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    private static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
 
     private boolean isLeapYear(int year) {
         return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? true : false;
@@ -12,16 +16,23 @@ public class Calendar {
         return isLeapYear(year) ? LEAP_MAX_DAYS[mon - 1] : MAX_DAYS[mon - 1];
     }
 
-    public void printCalendar(int year, int mon) {
+    public void printCalendar(int year, int mon, String day) {
+        System.out.printf("     <<%4d Year %s>>\n", year, MONTHS[mon - 1]);
         System.out.println("Sun Mon Tue Wed Thur Fri Sat");
         System.out.println("----------------------------");
 
         int max_day = getMaxDate(year, mon);
+        int start_day = Arrays.asList(DAYS).indexOf(day);
 
-        for (int i = 1; i < max_day + 1; i++) {
-            System.out.printf("%3d ", i);
-            if (i % 7 == 0) {
-                System.out.println();
+        for (int i = 1; i < max_day + 1 + start_day; i++) {
+
+            if (i < start_day + 1) {
+                System.out.print("    ");
+            } else {
+                System.out.printf("%3d ", i - start_day);
+                if (i % 7 == 0) {
+                    System.out.println();
+                }
             }
         }
     }
@@ -29,7 +40,7 @@ public class Calendar {
     public static void main(String[] args) {
         Calendar cal = new Calendar();
 
-        cal.printCalendar(2020, 2);
+        cal.printCalendar(2020, 2, "Sat");
         System.out.println(); 
     }
 }
