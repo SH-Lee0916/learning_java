6e1f9b9b823029ecd1436830b48423ef2c5c3a1f
GitHub-Joayo <dkssudxx@naver.com>
2022-06-22 01:59:34 +0900

    FEAT: Auto calculate start day
    Implemented startDay method, calculate num of days from standard date
    then return result of modulo 7. 0 is Sunday, 6 is Saturday. Standard
    date is 1 Year January first.

--- Calendar/Calendar.java
+++ Calendar/Calendar.java
@@ -1,12 +1,10 @@
 package Calendar;
 
-import java.util.Arrays;
-
 public class Calendar {
     private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
     private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30 ,31, 30, 31};
     private static final String[] MONTHS = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
-    private static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+    // private static final String[] DAYS = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
 
     private boolean isLeapYear(int year) {
         return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0) ? true : false;
@@ -16,13 +14,22 @@ public class Calendar {
         return isLeapYear(year) ? LEAP_MAX_DAYS[mon - 1] : MAX_DAYS[mon - 1];
     }
 
-    public void printCalendar(int year, int mon, String day) {
+    private int startDay(int year, int mon) {
+        int preYear = year - 1;
+        int numOfDays = preYear * 365 + (preYear/4 - preYear/100 + preYear/400);
+        for (int i = 0; i < mon - 1; i++) {
+            numOfDays += isLeapYear(year) ? LEAP_MAX_DAYS[i] : MAX_DAYS[i];
+        }
+        return (numOfDays + 1) % 7;
+    }
+
+    public void printCalendar(int year, int mon) {
         System.out.printf("     <<%4d Year %s>>\n", year, MONTHS[mon - 1]);
         System.out.println("Sun Mon Tue Wed Thur Fri Sat");
         System.out.println("----------------------------");
 
         int max_day = getMaxDate(year, mon);
-        int start_day = Arrays.asList(DAYS).indexOf(day);
+        int start_day = startDay(year, mon);
 
         for (int i = 1; i < max_day + 1 + start_day; i++) {
 
@@ -40,7 +47,7 @@ public class Calendar {
     public static void main(String[] args) {
         Calendar cal = new Calendar();
 
-        cal.printCalendar(2020, 2, "Sat");
+        cal.printCalendar(2022, 6);
         System.out.println(); 
     }
 }
