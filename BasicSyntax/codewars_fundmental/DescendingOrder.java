954c95b03dfbfbdebe6ee6a1269cad6916858ee8
GitHub-Joayo <dkssudxx@naver.com>
2022-06-24 01:44:50 +0900

    FEAT: Update new solution 06/24
    java.util.Arrays, Arrays.sort(), array reversing.

---
+++ BasicSyntax/codewars_fundmental/DescendingOrder.java
@@ -0,0 +1,21 @@
+package BasicSyntax.codewars_fundmental;
+import java.util.Arrays;
+
+public class DescendingOrder {
+
+    public static int sortDesc(final int num) {
+        char[] tmpNum = Integer.toString(num).toCharArray();
+        Arrays.sort(tmpNum);
+        char[] descNum = new char[tmpNum.length];
+        for (int i = 0; i < tmpNum.length; i++) {
+            descNum[i] = tmpNum[tmpNum.length - 1 - i];
+        }
+        return Integer.parseInt(new String(descNum));
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%d, %d\n", 0, DescendingOrder.sortDesc(0));
+        System.out.printf("%d, %d\n", 51, DescendingOrder.sortDesc(15));
+        System.out.printf("%d, %d\n", 987654321, DescendingOrder.sortDesc(123456789));
+    }
+}
