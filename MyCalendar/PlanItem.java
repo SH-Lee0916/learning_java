ee221ac8eee26c6da4e310a9175804006f3b4260
GitHub-Joayo <dkssudxx@naver.com>
2022-07-01 01:19:06 +0900

    Feat: Implement simple service and test code
    Implemented simple service and test code using Java Spring.

---
+++ MyCalendar/PlanItem.java
@@ -0,0 +1,7 @@
+package MyCalendar;
+
+import java.util.Date;
+
+public class PlanItem {
+    
+}
---
+++ MyCalendar/RunPrompt.java
@@ -0,0 +1,14 @@
+package MyCalendar;
+
+import java.util.Scanner;
+import MyCalendar.MyCalendar;
+
+public class RunPrompt {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        MyCalendar cal = new MyCalendar();
+
+        
+
+    }
+}
