f0ba5af4be0ff691afcdeacca965245e1c821ab3
GitHub-Joayo <dkssudxx@naver.com>
2022-07-02 14:45:28 +0900

    Feat: Add AOP for checking time
    @Aspect, @Around, AOP is very comfortable.

--- LearnSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ LearnSpring/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.MemberRepository;
 // import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -35,6 +36,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+    // @Bean
+    // public TimeTraceAop timeTraceAop() {
+    //     return new TimeTraceAop();
+    // }
 
     // @Bean
     // public MemberRepository memberRepository() {
