06d73d99f6032a60961439df92c6ae47c336cd2b
GitHub-Joayo <dkssudxx@naver.com>
2022-07-10 03:25:48 +0900

    Feat: Add AppConfig class
    Now It's meet DIP.

--- LearnSpring/core/src/main/java/hello/core/OrderApp.java
+++ LearnSpring/core/src/main/java/hello/core/OrderApp.java
@@ -1,17 +1,22 @@
 package hello.core;
 
+import hello.core.disount.FixDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+// import hello.core.member.MemberServiceImpl;
+// import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+// import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+        // MemberService memberService = new MemberServiceImpl(new MemoryMemberRepository());
+        // OrderService orderService = new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.OrderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "member1", Grade.VIP);
