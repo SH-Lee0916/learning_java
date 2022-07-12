c370c898a078d33676f6766e7dc3390b4824d3ae
GitHub-Joayo <dkssudxx@naver.com>
2022-07-13 01:12:43 +0900

    Feat: Add filter
    Filter testing.

--- LearnSpring/core/src/main/java/hello/core/AutoAppConfig.java
+++ LearnSpring/core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan( // To preserve example code
+@ComponentScan( // To preserve example code, added exclude filter.
+    // basePackages = "hello.core.member", // component scan package 지정 // default -> @ComponentScan이 붙은 설정 정보 클래스의 위치
+    basePackageClasses = AutoAppConfig.class,
     excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // @Configuration annotation 제외
 )
 public class AutoAppConfig {
