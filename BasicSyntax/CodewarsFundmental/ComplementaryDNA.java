7d4e022cafbe732f4a40b28bd970fd92ab4a840e
GitHub-Joayo <shlee0916.dev@gmail.com>
2022-07-16 00:22:02 +0900

    Feat: Edit solution

--- BasicSyntax/CodewarsFundmental/ComplementaryDNA.java
+++ BasicSyntax/CodewarsFundmental/ComplementaryDNA.java
@@ -1,9 +1,23 @@
 // https://www.codewars.com/kata/554e4a2f232cdd87d9000038/
 package BasicSyntax.CodewarsFundmental;
 
+import java.util.List;
+import java.util.stream.Stream;
 
 public class ComplementaryDNA {
-    
+    public String makeComplement(String dna) {
+        String resultStr = "";
+
+        for (int i = 0; i < dna.length(); i++) {
+            if (dna.charAt(i) == 'A') resultStr += 'T';
+            if (dna.charAt(i) == 'G') resultStr += 'C';
+            if (dna.charAt(i) == 'C') resultStr += 'G';
+            if (dna.charAt(i) == 'T') resultStr += 'A';
+        }
+
+        return resultStr;
+
+    }
 
 
     public static void main(String[] args) {
