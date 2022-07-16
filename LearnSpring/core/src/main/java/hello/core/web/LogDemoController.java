a4b93d6a9b0de6445b7ff6a7221756213a20469d
GitHub-Joayo <shlee0916.dev@gmail.com>
2022-07-16 23:56:13 +0900

    Feat: Add scope proxyMode
    SUPER POWER. proxyMode is amazing.

--- LearnSpring/core/src/main/java/hello/core/web/LogDemoController.java
+++ LearnSpring/core/src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,7 @@ package hello.core.web;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.ObjectProvider;
+// import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,14 +15,15 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
     
     private final LogDemoService LogDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURI().toString();
+
+        System.out.println("myLogger: " + myLogger.getClass());
 
         myLogger.setRequestURL(requestURL);
 
